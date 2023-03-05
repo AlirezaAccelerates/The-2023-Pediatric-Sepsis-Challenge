9a96330493fbd9f16a44eac7aa7b5b61f7d37c73
Alireza <96019816+AlirezaRafiei9@users.noreply.github.com>
2023-03-05 15:02:04 -0500

    Update team_code.py

--- example-python-20xx/team_code.py
+++ example-python-20xx/team_code.py
@@ -51,3 +51,13 @@ def train_challenge_model(data_folder, model_folder, verbose):
 
     if verbose >= 1:
         print('Done!')
+        
+# Load your trained models. This function is *required*. You should edit this function to add your code, but do *not* change the
+# arguments of this function.
+def load_challenge_models(model_folder, verbose):
+    print('Loading the model...')
+    filename = os.path.join(model_folder, 'models.sav')
+    return joblib.load(filename)
+
+
+
