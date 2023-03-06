78b7ac17f1354e491c423dd71308369c90f7ef6f
Alireza <96019816+AlirezaRafiei9@users.noreply.github.com>
2023-03-06 18:35:56 -0500

    Update run_model.py

--- example-python-20xx/run_model.py
+++ example-python-20xx/run_model.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python
+
+# Do *not* edit this script. Changes will be discarded so that we can run the trained models consistently.
+
+# This file contains functions for running models for the Challenge. You can run it as follows:
 #
+#   python run_model.py models data outputs
+#
+# where 'models' is a folder containing the your trained models, 'data' is a folder containing the Challenge data, and 'outputs' is a
+# folder for saving your models' outputs.
+
+import numpy as np, scipy as sp, os, sys
+from helper_code import *
+from team_code import load_challenge_models, run_challenge_models
