780da677616874923e69de13dee36e76718be576
Alireza <96019816+AlirezaRafiei9@users.noreply.github.com>
2023-08-04 10:56:35 -0400

    Update evaluate_model.py

--- evaluation-2023/evaluate_model.py
+++ evaluation-2023/evaluate_model.py
@@ -25,13 +25,11 @@ def evaluate_model(label_folder, output_folder):
     
     # Evaluate the models.
     tn, fp, fn, tp = compute_confusion_matrix(label, prediction_binary)
-    sensitivity = compute_sensitivity(tp, fp)
-    ppv = compute_ppv(tp, fn)
     accuracy = compute_accuracy(tn, fp, fn, tp)
 
-    score = challenge_score(sensitivity, ppv)
+    score = challenge_score(label, prediction_probability)  
 
-    return score, sensitivity, ppv, accuracy
+    return score, accuracy
 
 
 # Compute confusion matrix for model's predictions for the binary case
@@ -65,13 +63,86 @@ def compute_ppv(tp, fp):
 def compute_sensitivity(tp, fn):
     return tp / (tp + fn)
 
-# Compute challenge score .
-def challenge_score(sensitivity, ppv):
-    return min(sensitivity, ppv)
-
 # Compute accuracy.
 def compute_accuracy(tn, fp, fn, tp):
     return (tp + tn) / (tn + fp + fn + tp)
+
+# Compute challenge score .
+def compute_challenge_score(labels, outputs):
+    # Check the data.
+    assert len(labels) == len(outputs)
+
+    # Convert the data to NumPy arrays for easier indexing.
+    labels = np.asarray(labels, dtype=np.float64)
+    outputs = np.asarray(outputs, dtype=np.float64)
+
+    num_instances = len(labels)
+
+    # Collect the unique output values as the thresholds for the positive and negative classes.
+    thresholds = np.unique(outputs)
+    thresholds = np.append(thresholds, thresholds[-1]+1)
+    thresholds = thresholds[::-1]
+    num_thresholds = len(thresholds)
+
+    idx = np.argsort(outputs)[::-1]
+
+    # Initialize the TPs, FPs, FNs, and TNs with no positive outputs.
+    tp = np.zeros(num_thresholds)
+    fp = np.zeros(num_thresholds)
+    fn = np.zeros(num_thresholds)
+    tn = np.zeros(num_thresholds)
+
+    tp[0] = 0
+    fp[0] = 0
+    fn[0] = np.sum(labels == 1)
+    tn[0] = np.sum(labels == 0)
+
+    # Update the TPs, FPs, FNs, and TNs using the values at the previous threshold.
+    k = 0
+    for l in range(1, num_thresholds):
+        tp[l] = tp[l-1]
+        fp[l] = fp[l-1]
+        fn[l] = fn[l-1]
+        tn[l] = tn[l-1]
+
+        while k < num_instances and outputs[idx[k]] >= thresholds[l]:
+            if labels[idx[k]] == 1:
+                tp[l] += 1
+                fn[l] -= 1
+            else:
+                fp[l] += 1
+                tn[l] -= 1
+            k += 1
+
+        # Compute the FPRs.
+        fpr = np.zeros(num_thresholds)
+        for l in range(num_thresholds):
+            if tp[l] + fn[l] > 0:
+                fpr[l] = float(fp[l]) / float(tp[l] + fn[l])
+            else:
+                fpr[l] = float('nan')
+
+        # Find the threshold such that FPR <= 0.05.
+        max_fpr = 0.05
+        if np.any(fpr <= max_fpr):
+            l = max(l for l, x in enumerate(fpr) if x <= max_fpr)
+            tp = tp[l]
+            fp = fp[l]
+            fn = fn[l]
+            tn = tn[l]
+        else:
+            tp = tp[0]
+            fp = fp[0]
+            fn = fn[0]
+            tn = tn[0]
+
+    # Compute the TPR at FPR <= 0.05.
+    if tp + fn > 0:
+        max_tpr = tp / (tp + fn)
+    else:
+        max_tpr = float('nan')
+
+    return max_tpr
 
 
 if __name__ == "__main__":
@@ -80,13 +151,11 @@ if __name__ == "__main__":
     scores = evaluate_model(sys.argv[1], sys.argv[2])
     
     # Unpack the scores.
-    score, sensitivity, ppv, accuracy = scores
+    score, accuracy = scores
     
     # Construct a string with scores.
     output_string = \
         'Challenge Score: {:.3f}\n'.format(score) + \
-        'Sensitivity: {:.3f}\n'.format(sensitivity) + \
-        'Positive Predictive Value: {:.3f}\n'.format(ppv) + \
         'Accuracy: {:.3f}\n'.format(accuracy)
 
     
